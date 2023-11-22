d23a9d8d3009265a29a3eac51fd527d8431d0e6a
Joshua Biong <joshuabiong7@gmail.com>
2023-11-22 10:27:09 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Joshua P. Biong on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
