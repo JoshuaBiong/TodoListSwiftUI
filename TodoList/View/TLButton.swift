be5dd27740e810e40967dbf4a85370f77f7744e5
Joshua Biong <joshuabiong7@gmail.com>
2023-11-23 18:35:45 +0800

    Register View and make a button view and Header to reuse the code

---
+++ TodoList/View/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  TodoList
+//
+//  Created by Joshua P. Biong on 11/23/23.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
